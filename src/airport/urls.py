6751edb4c2e30e83c2d6555baf02bb80fccda9ee
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-02-22 22:43:02 +0200

    implement routing for API endpoints

--- src/airport/urls.py
+++ src/airport/urls.py
@@ -1,3 +1,29 @@
-app_name = "airport"
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from airport.views import (
+    AirportViewSet,
+    RouteViewSet,
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    FlightViewSet,
+    CrewViewSet,
+    CountryViewSet,
+    CityViewSet
+)
+
+router = DefaultRouter()
+router.register("airport", AirportViewSet)
+router.register("route", RouteViewSet)
+router.register("airplane_type", AirplaneTypeViewSet)
+router.register("airplane", AirplaneViewSet)
+router.register("flight", FlightViewSet)
+router.register("crew", CrewViewSet)
+router.register("country", CountryViewSet)
+router.register("city", CityViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "airport"
