cc1e8dd85e6c0e8e5b573699dcf2993ec026845d
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-02-23 20:35:23 +0200

    update routers in airport app urls

--- src/airport/urls.py
+++ src/airport/urls.py
@@ -9,20 +9,20 @@ from airport.views import (
     FlightViewSet,
     CrewViewSet,
     CountryViewSet,
-    CityViewSet, OrderViewSet, TicketViewSet
+    CityViewSet,
+    OrderViewSet
 )
 
 router = DefaultRouter()
-router.register("airport", AirportViewSet)
-router.register("route", RouteViewSet)
-router.register("airplane_type", AirplaneTypeViewSet)
-router.register("airplane", AirplaneViewSet)
-router.register("flight", FlightViewSet)
-router.register("crew", CrewViewSet)
-router.register("country", CountryViewSet)
-router.register("city", CityViewSet)
-router.register("order", OrderViewSet)
-router.register("ticket", TicketViewSet)
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("flights", FlightViewSet)
+router.register("crews", CrewViewSet)
+router.register("countries", CountryViewSet)
+router.register("cities", CityViewSet)
+router.register("orders", OrderViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
