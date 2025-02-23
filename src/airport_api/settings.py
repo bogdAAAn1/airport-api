e893c2a9b9e1e624bb29c6738b46887decd48c10
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-02-23 23:49:08 +0200

    add default pagination in rest settings

--- src/airport_api/settings.py
+++ src/airport_api/settings.py
@@ -144,7 +144,10 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
+    "MAX_PAGE_SIZE": 20,
 }
 
 SPECTACULAR_SETTINGS = {
