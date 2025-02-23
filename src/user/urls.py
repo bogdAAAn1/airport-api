ff19e1b6d3fd66f5519f4894a03029484f2acd0a
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-02-23 20:37:27 +0200

    update urls for user app

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,3 +1,10 @@
-app_name = "user"
+from django.urls import path
 
-urlpatterns = []
+from user.views import CreateUserView, ManageUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
+
+app_name = "user"
