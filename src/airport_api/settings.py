618cada61422920bf4192c165b76c41e58be87e6
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-02-25 17:11:10 +0200

    change database to postgre

--- src/airport_api/settings.py
+++ src/airport_api/settings.py
@@ -88,10 +88,14 @@ WSGI_APPLICATION = "airport_api.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   "default": {
+       "ENGINE": "django.db.backends.postgresql",
+       "NAME": os.environ["POSTGRES_DB"],
+       "USER": os.environ["POSTGRES_USER"],
+       "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+       "HOST": os.environ["POSTGRES_HOST"],
+       "PORT": int(os.environ["POSTGRES_DB_PORT"]),
+   }
 }
 
 # Password validation
